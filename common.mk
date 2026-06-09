e8361cbdde3b30a5eedb902930e40edcdc7a0be0
Cao Khoi <khoi22662@gmail.com>
2026-06-09 21:02:53 +0700

    change NFC service

--- common.mk
+++ common.mk
@@ -182,7 +182,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.sec \
+    android.hardware.nfc@1.2-service.samsung \
     com.android.nfc_extras \
     Tag
 
